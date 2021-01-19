5768b2c90c43e0c7da43153e7b320147bf520677
Fructus <j.fructus@it-students.fr>
2021-01-19 12:30:17 +0100

    css header

--- layout/header.php
+++ layout/header.php
@@ -3,19 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="SQL_Project\public\css\styles.css">
     <link rel="stylesheet" type="text/css" href="SQL_Project/public/css/main.css">
     <script src="SQL_Project/public/js/main.js" type="text/javascript"></script>
     <title>SuperGoat</title>
 </head>
 
 <body>
-            <h1>SuperGoat Hardware</h1>
+      <nav class="navbar">
+          <div class="navbar-title">SuperGoat Hardware</div>
+          <div class="navbar-links">
+            <ul>
+                 <li><a href="/">Home</a></li>
+                 <li><a href="signin">Sign In</a></li>
+                 <li><a href="register">Register</a></li>
+            </ul>
+          </div>
+       </nav>     
    
-             <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="signin">Sign In</a></li>
-                    <li><a href="register">Register</a></li>
-                </ul>
-            </nav>
+</body> 
+</html>            
     
--- view/register_view.php
+++ view/register_view.php
@@ -1,48 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="SQL_Project\public\css\styles.css">
+    <link rel="stylesheet" type="text/css" href="SQL_Project/public/css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    
 
-<div class = "header">
-    <h2>Bonjour, veuillez vous enregistrer sur Supergoat Hardware! :)</h2>
-</div>   
-            <form action="/register" method="post">
+ <div class = "header_title">
+                    <h2>Bonjour, veuillez vous enregistrer sur Supergoat Hardware! :)</h2>
+                </div>   
+                            <form action="/register" method="post">
 
-                <!-- affichage des erreurs -->
+                                <!-- affichage des erreurs -->
 
-                <?php include('../public/errors.php'); ?>
+                                <?php include('../public/errors.php'); ?>
 
-                <div class="input-group">
-                    <label>Prénom: </label>
-                    <input type="text" name="firstname"><br>
-                </div>
+                                <div class="input-group">
+                                    <label>Prénom: </label>
+                                    <input type="text" name="firstname"><br>
+                                </div>
 
-                <div class="input-group">
-                    <label>Nom: </label>
-                    <input type="text" name="lastname"><br>
-                </div>
+                                <div class="input-group">
+                                    <label>Nom: </label>
+                                    <input type="text" name="lastname"><br>
+                                </div>
 
-                <div class="input-group">
-                    <label>Email: </label>
-                    <input type="text" name="email"><br>
-                </div>
+                                <div class="input-group">
+                                    <label>Email: </label>
+                                    <input type="text" name="email"><br>
+                                </div>
 
-                <div class="input-group">
-                    <label>Mot de passe: </label>
-                    <input type="password" name="password_1"><br>
-                </div>
+                                <div class="input-group">
+                                    <label>Mot de passe: </label>
+                                    <input type="password" name="password_1"><br>
+                                </div>
 
-                <div class="input-group">
-                    <label>Répétez le mot de passe: </label>
-                    <input type="password" name="password_2"><br>
-                </div>
+                                <div class="input-group">
+                                    <label>Répétez le mot de passe: </label>
+                                    <input type="password" name="password_2"><br>
+                                </div>
 
-                <div class="input-group">
-                    <label>Téléphone: </label>
-                    <input type="text" name="phone"><br>
-                </div>    
+                                <div class="input-group">
+                                    <label>Téléphone: </label>
+                                    <input type="text" name="phone"><br>
+                                </div>    
 
-                <div class="input-group">
-                    <button type="submit" name="register" class="btn">Envoyer</button>
-                </div>
+                                <div class="input-group">
+                                    <button type="submit" name="register" class="btn">Envoyer</button>
+                                </div>
 
-                <p>Déja membre? <a href="signin_view.php"> Identifiez-vous </a>
-                </p>
+                                <p>Déja membre? <a href="signin_view.php"> Identifiez-vous </a>
+                                </p>
 
-            </form>
+                            </form>
+
+
+</body>
+
+</html>                            
